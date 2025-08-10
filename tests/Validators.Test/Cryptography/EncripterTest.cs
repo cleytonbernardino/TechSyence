bda0d27c74b12c990662074fd894a38d5fc1f3d7
Cleyton Bernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-08-09 22:17:31 -0300

    Teste de criptografia de senhas

---
+++ tests/Validators.Test/Cryptography/EncripterTest.cs
@@ -0,0 +1,19 @@
+﻿using Shouldly;
+using TechSyence.Infrastructure.Security.Cryptography;
+
+namespace Validators.Test.Cryptography;
+
+public class EncripterTest
+{
+    [Fact]
+    public void Success()
+    {
+        var encripter = new Sha512Encripter("TestingValue");
+
+        string password = "TestPassword";
+
+        var encriptedPassword = encripter.Encript(password);
+
+        encriptedPassword.ShouldNotBe(password);
+    }
+}
